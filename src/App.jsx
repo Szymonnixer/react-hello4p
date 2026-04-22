4b3abe734492ba20ae8d9e2fed25b2c417552390
Szymonnixer <szymongorsky408@wp.pl>
2026-04-22 15:59:41 +0200

    trzeci commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,12 @@
 import { useState } from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from './assets/vite.svg';
-import heroImg from './assets/hero.png';
+import Trzy from './App2.jsx'
 import './App.css';
 
 function Dwa(){
   return (
     <>
       <p>To jest przykład wywoływania funkcji: </p>
+      <Trzy />
     </>
   );
 }
---
+++ src/App2.jsx
@@ -0,0 +1,12 @@
+import { useState } from 'react';
+import './App.css';
+
+function Trzy(){
+    return (
+      <>
+        <p>To jest z osobnego pliku!!</p>
+      </>
+    );
+}
+
+export default Trzy;
